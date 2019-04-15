a3b68022407978cb392bdb66d98450a0a29de7d7
Rplus <rplus.tw@gmail.com>
2019-04-16 04:05:56 +0800

    update popup content

--- js/create-mark-content.js
+++ js/create-mark-content.js
@@ -1,23 +1,28 @@
 import { navigation, getPlanText } from './u/u.js';
 
 export default function createMarkerContent(report) {
+  let googleNavigation = navigation(
+    `${report.lat},${report.lng}`,
+    `${window.info.nowlatlng.lat},${window.info.nowlatlng.lng}`
+  );
 
   return `
     <div class="popup-content">
       #${report.dex} ${report.name}: ${report.scale}+/hr
       <div class="note">${getPlanText(report.note || '')}</div>
       <hr>
-      <div class="align-right">
-      <a class="fix-reoprt" href="###"
-        data-lat="${report.lat}"
-        data-lng="${report.lng}"
-        data-scale="${report.scale}"
-        data-name="${report.name}"
-        data-note="${report.note || ''}"
-        data-dex="${report.dex}"
-      >
-        修正更新
-      </a>
+      <div class="popup-content--footer">
+        <a class="fix-reoprt" href="###"
+          data-lat="${report.lat}"
+          data-lng="${report.lng}"
+          data-scale="${report.scale}"
+          data-name="${report.name}"
+          data-note="${report.note || ''}"
+          data-dex="${report.dex}"
+        >
+          更新👊
+        </a>
+        <a href="${googleNavigation}" target="_blank">Go↗️</a>
       </div>
     </div>
   `;
