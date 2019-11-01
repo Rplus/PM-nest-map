12212dddbf538a91ba906cefac016ffa302af1d3
Rplus <rplus.tw@gmail.com>
2019-11-01 17:31:42 +0800

    check data is outdated or not

--- js/get-data.js
+++ js/get-data.js
@@ -5,18 +5,30 @@ import { earseMarkers } from './map.js';
 import { renderMarkersInView } from './map.js';
 import { createNamesOption } from './dialog.js';
 
+const twoWeekinMs = 86400000 * 14;
+
+let checkOutdated = (data) => {
+  return (new Date() - new Date(data.timestamp)) > twoWeekinMs;
+};
+
+let checkLatLng = (data) => !(isNaN(data.lat) && isNaN(data.lng));
+
 export function getData() {
   console.log('re-fetching~~~');
 
-  let checkLatLng = (data) => !(isNaN(data.lat) && isNaN(data.lng));
 
   Promise.all([
     u.fetchJSON(urls.GAS),
     u.fetchJSON(`${urls.GAS}&method=get_pm_name`),
   ])
   .then(d => {
-    let data = d[0].data.filter(checkLatLng);
+    let data = d[0].data.filter(data => checkLatLng(data));
     let pmNames = d[1];
+    data.forEach(data => {
+      if (checkOutdated(data)) {
+        data.dex = 0.5;
+      }
+    });
 
     createNamesOption(pmNames);
     window.info.pmNames = pmNames;
